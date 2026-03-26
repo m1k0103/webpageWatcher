9e3e096efb18df286557a67b0aea5c5aeefc1b31
mikolaj <m1k0103@therockman.tech>
2026-03-26 19:19:24 +0000

    ID reader finished

---
+++ src/IdReader.java
@@ -0,0 +1,26 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.print.Doc;
+import java.io.IOException;
+
+public class IdReader extends GenericReader{
+    // Constructor
+    public IdReader(String website) {
+        super(website);
+    }
+
+    public String get(String id) throws IOException {
+        String rawPageData = super.get();
+        Document doc = Jsoup.parse(rawPageData);
+
+        Element el = doc.select(String.format("*#%s", id)).first();
+
+        // Check if element is found
+        if (el == null){
+            throw new RuntimeException(String.format("Element of id %s not found.", id));
+        }
+        return el.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,10 @@ public class Main {
         CssClassReader cssReader = new CssClassReader("https://therockman.tech/");
         System.out.println(cssReader.getAll("navbar-item")); // gets all of class
         System.out.println(cssReader.getFirst("navbar-item")); // gets only the first of class
+
+        // ID SELECTOR
+        IdReader idReader = new IdReader("https://therockman.tech/paste/paste.html");
+        System.out.println(idReader.get("input-box"));
     }
 }
 
